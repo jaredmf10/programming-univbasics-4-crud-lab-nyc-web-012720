4571bed72ef12f000ae8b49c3ddb1a1a51fa3ca8
Jared Fleming <jaredmf10@gmail.com>
2020-01-13 18:22:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  sticks = [bure, beezer, ovi]
-  sticks << sid
+  
 end
 
 def add_element_to_start_of_array(array, element)
